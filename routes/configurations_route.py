152500af64f7dc02ea79a15bb3e597e7e9274740
Ibrahim Akram Mughal <68148781+c1ph3rrs@users.noreply.github.com>
2024-12-14 00:11:58 +0400

    some modification done

--- routes/configurations_route.py
+++ routes/configurations_route.py
@@ -33,13 +33,27 @@ async def get_user_points(user_id: str):
 async def get_rewards_history(user_id: str):
     rewards_history = rewards_history_collection.find({"user_id": user_id})
     
-    if not rewards_history:
-        raise HTTPException(status_code=404, detail="Rewards history not found")
-    
     rewards_list = []
     for reward in rewards_history:
         reward["id"] = str(reward["_id"])
+        reward["datetime"] = reward["datetime"].isoformat()  # Convert datetime to ISO format string
         del reward["_id"]
         rewards_list.append(json.loads(json_util.dumps(reward)))
     
+    if not rewards_list:
+        raise HTTPException(status_code=404, detail="Rewards history not found")
+    
     return rewards_list
+
+
+@router.get("/user-records/{user_id}")
+async def get_user_records(user_id: str):
+    user_points = user_points_collection.find_one({"user_id": user_id})
+    
+    if not user_points:
+        raise HTTPException(status_code=404, detail="User points record not found")
+    
+    user_points["id"] = str(user_points["_id"])
+    del user_points["_id"]
+    
+    return json.loads(json_util.dumps(user_points))
